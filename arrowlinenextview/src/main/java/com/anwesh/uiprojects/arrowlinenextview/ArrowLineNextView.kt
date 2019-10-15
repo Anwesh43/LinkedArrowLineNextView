048332c8ada1dfe2f805ec0d75a8b8dc9646545b
Anwesh43 <anweshthecool0@gmail.com>
2019-10-15 23:07:22 +0530

    adding ALNNode class

--- arrowlinenextview/src/main/java/com/anwesh/uiprojects/arrowlinenextview/ArrowLineNextView.kt
+++ arrowlinenextview/src/main/java/com/anwesh/uiprojects/arrowlinenextview/ArrowLineNextView.kt
@@ -123,4 +123,46 @@ class ArrowLineNextView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALNNode(var i : Int, val state : State = State()) {
+
+        private var next : ALNNode? = null
+        private var prev : ALNNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ALNNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALNNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALNNode {
+            var curr : ALNNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
