fe6c9c35ce049e8642d4b2e435d4dbd1987aa4c8
Anwesh43 <anweshthecool0@gmail.com>
2019-10-15 08:06:57 +0530

    adding constants and libraries

--- arrowlinenextview/src/main/java/com/anwesh/uiprojects/arrowlinenextview/ArrowLineNextView.kt
+++ arrowlinenextview/src/main/java/com/anwesh/uiprojects/arrowlinenextview/ArrowLineNextView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.arrowlinenextview
 /**
  * Created by anweshmishra on 15/10/19.
  */
+
+import android.view.View
+import android.app.Activity
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
