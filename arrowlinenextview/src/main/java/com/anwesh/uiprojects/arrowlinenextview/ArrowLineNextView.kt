a851badba4e70bb7fd6daa25773ab19ac3b5499c
Anwesh43 <anweshthecool0@gmail.com>
2019-10-15 23:11:01 +0530

    adding ArrowLineNext class

--- arrowlinenextview/src/main/java/com/anwesh/uiprojects/arrowlinenextview/ArrowLineNextView.kt
+++ arrowlinenextview/src/main/java/com/anwesh/uiprojects/arrowlinenextview/ArrowLineNextView.kt
@@ -165,4 +165,28 @@ class ArrowLineNextView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArrowLineNext(var i : Int) {
+
+        private val root : ALNNode = ALNNode(0)
+        private var curr : ALNNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
