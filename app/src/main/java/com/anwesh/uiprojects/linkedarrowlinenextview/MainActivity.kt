8bcff2600ac91796926159ed9d6d0227a793c437
Anwesh43 <anweshthecool0@gmail.com>
2019-10-15 23:19:36 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedarrowlinenextview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedarrowlinenextview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedarrowlinenextview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.arrowlinenextview.ArrowLineNextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ArrowLineNextView.create(this)
     }
 }
